e97e755bfd7abbcb6b58a269fc2b2d14890d4d63
Dotse Raphael <raphaelgraceman@gmail.com>
2025-06-16 22:46:32 +0000

    Update

--- controllers/invController.js
+++ controllers/invController.js
@@ -183,6 +183,21 @@ invCont.updateInventoryView = async function (req, res, next) {
 
 
 /* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id);
+  const invData = await invModel.getInventoryByClassificationId(
+    classification_id
+  );
+  if (invData[0].inv_id) {
+    return res.json(invData);
+  } else {
+    next(new Error("No data returned"));
+  }
+};
+
+/* ***************************
  *  Process Update Inventory Data
  * ************************** */
 invCont.updateInventory = async function (req, res, next) {
